8f323114bd6a492d82d4a67086f7b850b686046a
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2024-11-16 01:11:52 +0100

    submittion for mozilla addon store

--- index.js
+++ index.js
@@ -1,36 +1,136 @@
-// ==UserScript==
-// @name         Gen-Z Wikipedia
-// @namespace    http://tampermonkey.net/
-// @version      2024-11-14
-// @description  ultimate brainrot
-// @author       Twig
-// @match        https://*.wikipedia.org/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=wikipedia.org
-// @grant        none
-// ==/UserScript==
-
-
 const enabled_videos = [
-    "minecraft",
-    "small_subway"
+    "subway",
+    "minecraft_parkour"
 ]
 
 const videos = {
-    "minecraft": "https://github.com/JustTemmie/gen-Z-friendly-wikipedia/raw/refs/heads/master/video-files/minecraft.mp4",
-    "subway": "https://github.com/JustTemmie/gen-Z-friendly-wikipedia/raw/refs/heads/master/video-files/subway.mp4",
-    "discord_subway": "https://cdn.discordapp.com/attachments/1266118108154695730/1306725752699490464/3_Minute_Tiktok_Background_Gameplay_fast_w_audio_FXckmIoiIBs.mp4?ex=6737b6b1&is=67366531&hm=f7e2baa9eef33009670ff0725f22c8f59e35b89133c3b0141e0a1e38722db766&",
-    "small_subway": "https://cdn.discordapp.com/attachments/919668170812440607/1306748845186613268/video.mp4?ex=6737cc32&is=67367ab2&hm=0ad58afbfa5a8d6cc733d1f404292405c97ad7466b152ae6f0d47a3d9ed3d299&88",
-    "csgo": "https://skibidi.cloud/s/gQYqYkpQ8L3sCwS/download/csgo.mp4",
+    "minecraft_parkour": [
+        "https://files.catbox.moe/wnayaw.mp4",
+        "https://files.catbox.moe/ayvfnd.mp4",
+        "https://files.catbox.moe/tqcp2u.mp4"
+    ],
+    "subway": [
+        "https://files.catbox.moe/hu9qz8.mp4",
+        "https://files.catbox.moe/xduow5.mp4",
+        "https://files.catbox.moe/q85tcc.mp4"
+    ],
+    "parkour_civilization": [
+        "https://files.catbox.moe/i511z6.mp4",
+        "https://files.catbox.moe/7okyku.mp4",
+    ],
+    "mobile_game_ads": [
+        "https://files.catbox.moe/easj40.mp4",
+        "https://files.catbox.moe/nvvg22.mp4"
+    ]
 }
 
-console.log("injecting brainrot...")
+main()
 
-enabled_videos.forEach(function(video_ID, index) {
-    if (videos[video_ID] != undefined) {
-        setupVideo(["left", "right"][index], video_ID, false)
+function main() {
+    console.log("injecting brainrot...")
+    
+    document.body.style.fontWeight = "bold"
+
+    enabled_videos.forEach(function(video_ID, index) {
+        if (videos[video_ID] != undefined) {
+            setupVideo(["left", "right"][index], video_ID, false)
+        }
+    });
+    
+    if (!window.location.href.includes("Main_Page")) {
+        (async () => {injectTTSAudio()})()
     }
-});
+}
 
+async function generateTTSAudio(text) {
+    return await fetch("https://tiktok-tts.weilbyte.dev/api/generate", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "text": text,
+            "voice": "en_us_001"
+        }),
+    });
+}
+
+async function injectTTSAudio() {
+    function getTextBetweenHeaders(first_header, second_header) {
+        let text_nodes = []
+
+        let current_node = first_header
+        while (current_node != undefined && current_node != second_header) {
+            if (current_node.nodeName === "P" || current_node.nodeName === "UL") {
+                // be sure to remove the citations
+                text_nodes.push(current_node.textContent.replace(/\[\d+\]/g, ""))
+            }
+
+            current_node = current_node.nextSibling
+        }
+
+        return text_nodes.join(".")
+    }
+
+    const headers = document.querySelectorAll("h2")
+
+    for (let loop_index = 0; loop_index < headers.length; loop_index ++) {
+        const current_header = headers[loop_index]
+        const next_header = headers[loop_index + 1]
+
+        if (current_header.innerText == "References" || current_header.innerText == "External links") {
+            break
+        }
+        
+        let audio = document.createElement("audio")
+        audio.setAttribute("controls", "")
+        
+        current_header.insertAdjacentElement("beforeend", document.createElement("br"))
+        current_header.insertAdjacentElement("beforeend", audio)
+        
+        audio.addEventListener("play", async () => {
+            if (audio.source == undefined) {
+                
+                const text = getTextBetweenHeaders(current_header.parentNode, next_header.parentNode)
+                const text_segments = []
+                // due to the TTS API only being able to generate ~6 minutes of audio we split the text into 4000 character chunks
+                for (let text_index = 0; text_index < text.length; text_index += 4000) {
+                    text_segments.push(text.slice(text_index, text_index+4000))
+                }
+                
+                for (let chunk_index = 0; chunk_index < text_segments.length; chunk_index++) {
+                    // generate a new audio player if this isn't the first 4000 characters
+                    if (chunk_index != 0) {
+                        audio = document.createElement("audio")
+                        audio.setAttribute("controls", "")
+                        
+                        current_header.insertAdjacentElement("beforeend", document.createElement("br"))
+                        current_header.insertAdjacentElement("beforeend", audio)
+                    }
+
+                    const toast_ID = getRandomID()
+                    createToast(toast_ID, "Generating Audio...")
+
+                    const request = await generateTTSAudio(text_segments[chunk_index])
+                    
+                    if (request.status == 200) {
+                        const source = document.createElement("source")
+                        source.setAttribute("src", URL.createObjectURL(await request.blob()))
+                        source.setAttribute("type", "audio/mpeg")
+                        audio.appendChild(source)
+                        audio.source = source
+
+                        removeToast(toast_ID, 1000)
+                    } else {
+                        updateToast(toast_ID, `status code ${request.status} met whilst trying to generate audio`)
+                        removeToast(toast_ID, 5000)
+                        console.log(`${request.status} - ${request.response}`)
+                    }
+                }
+            }
+        })
+    }
+}
 async function moveArticle() {
     let article = document.getElementsByClassName("mw-page-container")[0]
 
@@ -112,64 +212,65 @@ function injectVideoPlayer(position, video_URL, audio_enabled) {
         video.currentTime = Math.random() * video.duration;
         moveArticle()
     });
-    
-    document.body.appendChild(video)
 
+    console.log(`${video_URL} has been injected`)
+    document.body.appendChild(video)
 }
 
 async function setupVideo(position, video_ID, audio_enabled) {
-    const video_URL = videos[video_ID]
+    const video_category = videos[video_ID]
+    const cached_videos = []
+    let queued_downloads = 0
+
+
     const db = await openDB();
 
-    if (await isCacheDownloading(db, video_ID)) {
-        let toast_ID = getRandomID()
-        createToast(toast_ID, `a background tab seems to be already downloading the ${video_ID} video\nthe video will be streamed on this page in the mean-time`)
-        removeToast(toast_ID, 5000)
-
-        injectVideoPlayer(position, video_URL, audio_enabled);
-
-        return
-    }
-    
-    const cachedBlob = await getCachedVideo(db, video_ID);
-    if (cachedBlob) {
-        console.log(`cached video exists for ${video_ID}`);
-        injectVideoPlayer(position, URL.createObjectURL(cachedBlob), audio_enabled);
-    } else {
-        try {
-            console.log(`fetching and caching video ${video_ID} video`);
-            await markCacheAsDownloading(db, video_ID)
-            
-            let not_in_cache_toast_ID = getRandomID()
-            createToast(not_in_cache_toast_ID, `the ${video_ID} video doesn't exist in cache so we need to download it, this will likely take multiple minutes...`)
-            removeToast(not_in_cache_toast_ID, 15000)
-
-            let dont_close_toast_ID = getRandomID()
-            createToast(dont_close_toast_ID, `do NOT close this tab until the video is playing, thanks`)
-            removeToast(dont_close_toast_ID, 15000)
-
-            const blob = await fetchWithProgress(video_URL, video_ID)
-
-            console.log(`i got a blob for ${video_ID} :3`)
-            cacheVideo(db, video_ID, blob);
-
-            let successfully_cached_toast_ID = getRandomID()
-            createToast(successfully_cached_toast_ID, `the ${video_ID} video has successfully been cached!`)
-            removeToast(successfully_cached_toast_ID, 5000)
-            console.log(`the ${video_ID} video has successfully been cached!`)
-
-            injectVideoPlayer(position, URL.createObjectURL(blob), audio_enabled);
-        } catch {
-            await markCacheAsNotDownloading(db, video_ID)
-            console.log(`failed to cache the ${video_ID} video, deciding to stream it instead...`)
-            injectVideoPlayer(position, video_URL, audio_enabled);
+    for (let index = 0; index < video_category.length; index ++) {
+        const video_URL = video_category[index]
+        const cachedBlob = await getCachedVideo(db, video_URL);
+        if (cachedBlob) {
+            cached_videos.push(video_URL)
+        } else {
+            if (queued_downloads < 1 && !await isCacheDownloading(db, video_URL)) {
+                queued_downloads++
+                (async () => {
+                    try {
+                        console.log(`fetching and caching video ${video_URL} video`);
+                        await markCacheAsDownloading(db, video_URL)
+                        
+                        const blob = await fetchWithProgress(video_URL)
+        
+                        console.log(`i got a blob for ${video_URL} :3`)
+                        cacheVideo(db, video_URL, blob);
+        
+                        let successfully_cached_toast_ID = getRandomID()
+                        createToast(successfully_cached_toast_ID, `the ${video_URL} video has successfully been cached!`)
+                        removeToast(successfully_cached_toast_ID, 5000)
+                        console.log(`the ${video_URL} video has successfully been cached!`) 
+        
+                        if (cached_videos.length == 0) {
+                            cached_videos.push(video_URL)
+                            injectVideoPlayer(position, URL.createObjectURL(blob), audio_enabled)
+                        }
+                    } catch {
+                        console.log(`error encountered whilst trying to download ${video_URL}`)
+                        markCacheAsNotDownloading(db, video_URL)
+                    }
+                })()
+            }
         }
+    }
+
+    if (cached_videos.length >= 1) {
+        const video_URL = cached_videos[Math.floor(Math.random() * cached_videos.length)]
+        const cachedBlob = await getCachedVideo(db, video_URL);
+        injectVideoPlayer(position, URL.createObjectURL(cachedBlob), audio_enabled)
     }
 }
 
 async function openDB() {
     return new Promise((resolve, reject) => {
-        const request = indexedDB.open("genZWikiVideoDB", 1);
+        const request = indexedDB.open("SigmapediaVideoDB", 1);
 
         request.onupgradeneeded = function(event) {
             const db = event.target.result;
@@ -260,7 +361,7 @@ async function cacheVideo(db, ID, blob) {
     store.put({id: ID, blob: blob})
 }
 
-async function fetchWithProgress(url, video_ID = "?") {
+async function fetchWithProgress(url) {
     const response = await fetch(url);
     
     const contentLength = response.headers.get('Content-Length');
@@ -278,24 +379,23 @@ async function fetchWithProgress(url, video_ID = "?") {
     
     let last_toast = Date.now()
     let progressbar_toast_ID = getRandomID()
-    createToast(progressbar_toast_ID, `downloading ${video_ID} video...`)
+    createToast(progressbar_toast_ID, `downloading ${url} video...`)
 
     while(true) {
         const { done, value } = await reader.read();
         if (done) {
-            updateToast(progressbar_toast_ID, `successfully finished downloading ${video_ID}!`)
-            removeToast(progressbar_toast_ID, 3000)
+            removeToast(progressbar_toast_ID, 900)
             break;
         }
 
         chunks.push(value);
         bytesReceived += value.length;
 
-        if (Date.now() - last_toast > 1000) {
+        if (Date.now() - last_toast > 5000) {
             if (totalSize != 0) {
-                updateToast(progressbar_toast_ID, `downloading ${video_ID}, ${(bytesReceived / totalSize * 100).toFixed(2)}% complete (${formatBytes(bytesReceived)} / ${formatBytes(totalSize)} recieved)`)
+                updateToast(progressbar_toast_ID, `downloading ${url}, ${(bytesReceived / totalSize * 100).toFixed(2)}% complete (${formatBytes(bytesReceived)} / ${formatBytes(totalSize)} recieved)`)
             } else {
-                updateToast(progressbar_toast_ID, `downloading ${video_ID}, (${formatBytes(bytesReceived)} / ? bytes recieved)`)
+                updateToast(progressbar_toast_ID, `downloading ${url}, (${formatBytes(bytesReceived)} / ? bytes recieved)`)
             }
 
             last_toast = Date.now()
@@ -320,6 +420,10 @@ function debounce(func, wait) {
     };
 }
 
+function getRandomID() {
+    return (Math.random() + 1).toString(36).substring(2)
+}
+
 function createToast(ID, message) {
     let toastContainer = document.getElementById('toast-container');
     if (!toastContainer) {
@@ -334,11 +438,11 @@ function createToast(ID, message) {
 
     const toast = document.createElement('div');
     toast.style.background = 'rgba(0, 0, 0, 1)';
-    toast.style.opacity = 0.9
-    toast.style.fontSize = "18px";
+    toast.style.opacity = 0.8
+    toast.style.fontSize = "14px";
     toast.style.color = 'white';
-    toast.style.padding = '20px';
-    toast.style.marginTop = '20px';
+    toast.style.padding = '14px';
+    toast.style.marginTop = '14px';
     toast.style.borderRadius = '5px';
     toast.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
     toast.id = ID
@@ -363,14 +467,10 @@ function removeToast(ID, timeout) {
         }
     }
 
-    setTimeout(() => {fadeOut(toast)}, timeout - 900)
+    setTimeout(() => {fadeOut(toast)}, timeout - 800)
     setTimeout(() => {
         toastContainer.removeChild(toast);
     }, timeout);
-}
-
-function getRandomID() {
-    return (Math.random() + 1).toString(36).substring(2)
 }
 
 // code fetched from https://stackoverflow.com/a/18650828
@@ -385,6 +485,7 @@ function formatBytes(bytes, decimals = 2) {
 
     return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
+
 
 const debouncedArticleMover = debounce(moveArticle, 100);
 
